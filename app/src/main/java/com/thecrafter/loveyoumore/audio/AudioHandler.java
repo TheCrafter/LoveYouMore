1a92657727b8d42e8017aa074beaf94ff1f7e87c
TheCrafter <dimitrisvlh@gmail.com>
2015-02-26 03:04:09 +0200

    Added javadoc comments to classes of com.thecrafter.loveyoumore.audio package

--- app/src/main/java/com/thecrafter/loveyoumore/audio/AudioHandler.java
+++ app/src/main/java/com/thecrafter/loveyoumore/audio/AudioHandler.java
@@ -8,6 +8,13 @@ import java.util.Random;
 
 /**
  * Created by TheCrafter on 20/2/2015.
+ *
+ * AudioHandler is responsible for Audio playing. It manages the random access to each
+ * audio resource. Every time update() is called the state of the music is changed.
+ * If the music is already playing, it stops and rolls back to start while if there is no
+ * music playing, the next audio starts.
+ * Next audio is always random with a simple limitation that the an audio cannot be played
+ * if it has been already played between the last "mLastPlayedAudiosSize" audios.
  */
 public class AudioHandler {
 
@@ -17,7 +24,9 @@ public class AudioHandler {
     // Index in mAudioVector for the next audio to play.
     private int mNextAudioIndex;
 
+    // An array with all last played audios that will affect the random select.
     private int[] mLastPlayedAudios;
+    // Size of mLastPlayedAudios.
     private final int mLastPlayedAudiosSize = 3;
 
     // Application context from the app that owns this AudioHandler.
@@ -27,6 +36,14 @@ public class AudioHandler {
     private Random mRand;
 
 
+    /**
+     * The only constructor of the class. Gets a vector with resources and the
+     * context of the application. Then initializes class variables. mLastPlayedAudios
+     * starts with all elements set to -1.
+     *
+     * @param vec A vector with audio resources to play.
+     * @param context The context of the application.
+     */
     public AudioHandler(Vector<AudioWrapper> vec, Context context){
         this.mAudioVector = vec;
         this.mAppContext = context;
@@ -40,10 +57,25 @@ public class AudioHandler {
             mLastPlayedAudios[i] = -1;
     }
 
+    /**
+     * Generates a random int within a specific range.
+     * @param min The minimum int to generate.
+     * @param max The maximum int to generate.
+     * @return The random generated int.
+     */
     private int getRandomInt(int min, int max){
         return mRand.nextInt((max - min) + 1) + min;
     }
 
+
+    /**
+     * Appends an index of an audio file from mAudioVector to last played audios array. This is
+     * done by shifting the contents of the array to the left and getting rid of the last element.
+     * Example:
+     * We have [5,4,6,8] and we want to append 1.
+     * Result: [1,5,4,6].
+     * @param index Index of audio file from mAudioVector to append.
+     */
     private void appendToLastPlayedAudios(int index){
         for (int i = 0; i < mLastPlayedAudiosSize; i++)
             if (i == mLastPlayedAudiosSize - 1)
@@ -52,6 +84,12 @@ public class AudioHandler {
                 mLastPlayedAudios[i + 1] = mLastPlayedAudios[i];
     }
 
+    /**
+     * Check if an index of an audio file from mAudioVector exists in last played audios array
+     * or not.
+     * @param index Index from an audio file from mAudioVector to check if exists.
+     * @return True if exists, False otherwise.
+     */
     private boolean existInLastPlayedAudios(int index){
         for(int i = 0; i < mLastPlayedAudiosSize - 1; i++)
             if (mLastPlayedAudios[i] == index)
@@ -60,10 +98,13 @@ public class AudioHandler {
         return false;
     }
 
+    /**
+     * Randomly decides what the next audio to play should be.
+     */
     private void updateNextAudioIndex(){
         int nextIndex;
 
-        // Exclude the previously played audio
+        // Exclude the previously played audio files.
         do{
             nextIndex = getRandomInt(0, mAudioVector.size() - 1);
         }while(existInLastPlayedAudios(nextIndex));
@@ -72,6 +113,10 @@ public class AudioHandler {
         mNextAudioIndex = nextIndex;
     }
 
+    /**
+     * Get the index of the currently playing resource or -1 if no music is playing.
+     * @return The index of the music playing or -1 if no music is playing.
+     */
     private int getCurPlayingIndex(){
         for(int i = 0; i < mAudioVector.size(); i++)
             if(mAudioVector.elementAt(i).isMusicPlaying())
@@ -80,6 +125,10 @@ public class AudioHandler {
         return -1;
     }
 
+    /**
+     * Updates the state of AudioHandler. If the music is stopped, starts a new track but if the
+     * music is playing, stops it.
+     */
     public void update(){
         int curPlayingIndex = getCurPlayingIndex();
 
--- app/src/main/java/com/thecrafter/loveyoumore/audio/AudioWrapper.java
+++ app/src/main/java/com/thecrafter/loveyoumore/audio/AudioWrapper.java
@@ -7,19 +7,34 @@ import java.io.IOException;
 
 /**
  * Created by TheCrafter on 19/2/2015.
+ *
+ * This class is a resource wrapper for audio resources. Has functions to handle the stop and play
+ * and restart of audio files.
  */
 public class AudioWrapper {
 
+    // The media player for this resource.
     private MediaPlayer mMediaPlayer;
+
+    // Flag to determine if music is playing or not.
     private boolean mMusicPlaying;
+
+    // Id if the audio resource.
     private int mResId;
 
+    /**
+     * The only constructor of the class. Gets the context of the application and the id of
+     * the resource you want to load in this AudioWrapper.
+     * @param context The context of the application.
+     * @param resid The id of the resource to load on this AudioWrapper.
+     */
     public AudioWrapper(Context context, int resid){
         this.mResId = resid;
 
         mMediaPlayer = MediaPlayer.create(context, mResId);
         mMusicPlaying = false;
 
+        // When track is complete, change playing flag (mMusicPlaying) to false.
         mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
@@ -28,6 +43,9 @@ public class AudioWrapper {
         });
     }
 
+    /**
+     * Starts the track. If the track is already playing, does nothing.
+     */
     public void play(){
         if(mMusicPlaying)
             return;
@@ -37,6 +55,11 @@ public class AudioWrapper {
         mMediaPlayer.start();
     }
 
+    /**
+     * Stops the track and returns it to start. If the track is not playing, does nothing.
+     * @throws IllegalStateException IllegalStateException thrown by MediaPlayer::prepare().
+     * @throws IOException IOException thrown by MediaPlayer::prepare().
+     */
     public void stop() throws IllegalStateException, IOException{
         if(!mMusicPlaying)
             return;
@@ -51,11 +74,19 @@ public class AudioWrapper {
         mMediaPlayer.seekTo(0);
     }
 
+    /**
+     * Recreates the track.
+     * @param context The context of the application.
+     */
     public void recreate(Context context){
         mMediaPlayer = MediaPlayer.create(context, mResId);
         mMusicPlaying = false;
     }
 
+    /**
+     * Check if the music is currently playing or not.
+     * @return True if music is already playing, False otherwise.
+     */
     public boolean isMusicPlaying(){
         return mMusicPlaying;
     }
