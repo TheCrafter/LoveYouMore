1a13c46fe8d3fdf610e0133edf5b221dca502c70
TheCrafter <dimitrisvlh@gmail.com>
2016-12-30 04:48:23 +0200

    Added dummy third mode (yellow heart)

--- app/src/main/java/com/thecrafter/loveyoumore/MainActivity.java
+++ app/src/main/java/com/thecrafter/loveyoumore/MainActivity.java
@@ -21,30 +21,37 @@ import java.util.Vector;
 
 public class MainActivity extends Activity {
 
-    /** Object to manage audio */
     private AudioHandler mAudioHandler;
-
-    /** Array with all messages to appear on screen. Messages are loaded from xml.  */
+    private AudioHandler mAudioHandler2;
     private String[] mMsgArray;
-
-    /** Used to generate random indexes for mMsgArray. */
     private RandomIntGenerator mMsgIndexGenerator;
 
-    /** A shared toast to show simple messages */
-    private Toast mToast;
-
-    /** If this is true, pressing the heart will play a sound, otherwise show message. */
-    private boolean mMusicModeOn = true;
+    private Toast mToast; // Shared toast to show simple messages
+    private MediaPlayer mClickSoundPlayer; // Media player for onClick sounds
 
     // Animations
-    /** Bumping animation */
     private Animation mBumpAnimation;
-
-    /** Rotation animation */
     private Animation mRotateAnimation;
+    private Animation mFadeAnimation;
 
-    /** Media player for onClick sounds */
-    private MediaPlayer mClickSoundPlayer;
+    // Modes
+    private enum Mode {
+        MUSIC1,
+        TEXT1,
+        MUSIC2
+    }
+    private Mode mMode = Mode.MUSIC1;
+
+    private void nextMode() {
+        Mode next;
+        switch (mMode) {
+            case MUSIC1: next = Mode.TEXT1; break;
+            case TEXT1:  next = Mode.MUSIC2; break;
+            case MUSIC2: next = Mode.MUSIC1;  break;
+            default:     next = Mode.MUSIC1;
+        }
+        mMode = next;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,38 +59,63 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         // Init onClick Sound Player
-        mClickSoundPlayer = MediaPlayer.create(getApplicationContext(), R.raw.buttonclicksound);
+        mClickSoundPlayer = MediaPlayer.create(this, R.raw.buttonclicksound);
 
         // Init animations
-        mBumpAnimation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.heart_scaleup_anim);
-        mRotateAnimation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.heart_rotate_anim);
+        mBumpAnimation = AnimationUtils.loadAnimation(this, R.anim.heart_scaleup_anim);
+        mRotateAnimation = AnimationUtils.loadAnimation(this, R.anim.heart_rotate_anim);
+        mFadeAnimation = AnimationUtils.loadAnimation(this, R.anim.heart_fade_anim);
 
-        // Create audio vector and fill it with sound resources
+        // Create audio vectors and fill them with sound resources
         Vector<AudioWrapper> audioVector = new Vector<>();
+        audioVector.add(new AudioWrapper(this, R.raw.msg1));
+        audioVector.add(new AudioWrapper(this, R.raw.msg2));
+        audioVector.add(new AudioWrapper(this, R.raw.msg3));
+        audioVector.add(new AudioWrapper(this, R.raw.msg4));
+        audioVector.add(new AudioWrapper(this, R.raw.msg5));
+        audioVector.add(new AudioWrapper(this, R.raw.msg6));
+        audioVector.add(new AudioWrapper(this, R.raw.msg7));
+        audioVector.add(new AudioWrapper(this, R.raw.msg8));
+        audioVector.add(new AudioWrapper(this, R.raw.msg9));
+        audioVector.add(new AudioWrapper(this, R.raw.msg10));
+        audioVector.add(new AudioWrapper(this, R.raw.msg11));
+        audioVector.add(new AudioWrapper(this, R.raw.msg12));
+        audioVector.add(new AudioWrapper(this, R.raw.msg13));
+        audioVector.add(new AudioWrapper(this, R.raw.msg14));
+        audioVector.add(new AudioWrapper(this, R.raw.msg15));
+        audioVector.add(new AudioWrapper(this, R.raw.msg16));
+        audioVector.add(new AudioWrapper(this, R.raw.msg17));
+        audioVector.add(new AudioWrapper(this, R.raw.msg18));
+        audioVector.add(new AudioWrapper(this, R.raw.msg19));
+        audioVector.add(new AudioWrapper(this, R.raw.msg20));
 
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg1));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg2));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg3));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg4));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg5));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg6));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg7));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg8));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg9));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg10));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg11));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg12));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg13));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg14));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg15));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg16));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg17));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg18));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg19));
-        audioVector.add(new AudioWrapper(getApplicationContext(), R.raw.msg20));
+        /*
+        Vector<AudioWrapper> audioVector2 = new Vector<>();
+        audioVector2.add(new AudioWrapper(this, R.raw.msg21));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg22));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg23));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg24));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg25));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg26));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg27));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg28));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg29));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg30));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg31));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg32));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg33));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg34));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg35));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg36));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg37));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg38));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg39));
+        audioVector2.add(new AudioWrapper(this, R.raw.msg40));
+        */
 
-        // Pass the vector to audio handler
-        mAudioHandler = new AudioHandler(audioVector, getApplicationContext());
+        // Pass the vector to audio handlers
+        mAudioHandler = new AudioHandler(audioVector, this);
+        //mAudioHandler2 = new AudioHandler(audioVector2, this);
 
         // Get message array
         mMsgArray = getResources().getStringArray(R.array.msg_array);
@@ -102,19 +134,27 @@ public class MainActivity extends Activity {
             public boolean onLongClick(View v) {
 
                 // Vibrate for 50 millisecond
-                Vibrator vibe = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                Vibrator vibe = (Vibrator) MainActivity.this.getSystemService(Context.VIBRATOR_SERVICE);
                 vibe.vibrate(50);
 
                 ImageView heartImage = (ImageView) findViewById(R.id.heart_img);
 
-                // Change image
-                if (mMusicModeOn)
-                    heartImage.setImageResource(R.drawable.heart_purple);
-                else
-                    heartImage.setImageResource(R.drawable.heart);
-
                 // Change mode
-                mMusicModeOn = !mMusicModeOn;
+                nextMode();
+
+                // Change image
+                switch (mMode) {
+                    case MUSIC1:
+                        heartImage.setImageResource(R.drawable.heart);
+                        break;
+                    case MUSIC2:
+                        heartImage.setImageResource(R.drawable.heart_yellow);
+                        break;
+                    case TEXT1:
+                        heartImage.setImageResource(R.drawable.heart_purple);
+                        break;
+                }
+
                 return true;
             }
         });
@@ -122,38 +162,50 @@ public class MainActivity extends Activity {
 
     public void onImageClick(View v) {
 
-        // Check if we are on music mode or not
-        if(mMusicModeOn){
-            // Start bumping animation for heart image
-            v.startAnimation(mBumpAnimation);
+        switch (mMode) {
+            case MUSIC1: {
+                // Start bumping animation for heart image
+                v.startAnimation(mBumpAnimation);
 
-            // Update music state
-            mAudioHandler.update();
-        }
-        else{
-            // Play a System Sound
-            mClickSoundPlayer.stop();
-            try {
-                mClickSoundPlayer.prepare();
+                // Update music state
+                mAudioHandler.update();
+                break;
             }
-            catch (IOException e){
-                System.out.println(e.toString());
+
+            case MUSIC2: {
+                v.startAnimation(mFadeAnimation);
+
+                // Update music state
+                //mAudioHandler2.update();
+                break;
             }
-            mClickSoundPlayer.seekTo(0);
-            mClickSoundPlayer.start();
 
-            // Start rotate animation for heart image
-            v.startAnimation(mRotateAnimation);
+            case TEXT1: {
+                // Play a System Sound
+                mClickSoundPlayer.stop();
+                try {
+                    mClickSoundPlayer.prepare();
+                }
+                catch (IOException e){
+                    System.out.println(e.toString());
+                }
+                mClickSoundPlayer.seekTo(0);
+                mClickSoundPlayer.start();
 
-            // Show the next message
-            if(mToast != null)
-                mToast.cancel();
+                // Start rotate animation for heart image
+                v.startAnimation(mRotateAnimation);
 
-            mToast = Toast.makeText(getApplicationContext(), mMsgArray[mMsgIndexGenerator.getNextInt()], Toast.LENGTH_LONG);
-            mToast.show();
+                // Show the next message
+                if(mToast != null)
+                    mToast.cancel();
 
-            // Update the next message
-            mMsgIndexGenerator.updateNextInt();
+                mToast = Toast.makeText(this, mMsgArray[mMsgIndexGenerator.getNextInt()], Toast.LENGTH_LONG);
+                mToast.show();
+
+                // Update the next message
+                mMsgIndexGenerator.updateNextInt();
+                break;
+            }
         }
     }
 }
